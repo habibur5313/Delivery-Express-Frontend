4f102ed4b96ec7b7f78e599cc1f3765565f16abe
habibur5313 <habiburmamun313@gmail.com>
2025-08-25 01:20:53 -0600

    delivery history page complete

--- src/pages/receiver/viewDeliveryHistory.tsx
+++ src/pages/receiver/viewDeliveryHistory.tsx
@@ -1,7 +1,24 @@
-const viewDeliveryHistory = () => {
-  return (
-    <div>viewDeliveryHistory</div>
-  )
-}
+import { ParcelCard } from "@/components/modules/Parcel/ParcelCard";
+import { useGetDeliveredParcelsQuery } from "@/redux/features/Parcel/parcel.api";
 
-export default viewDeliveryHistory;
+const ViewDeliveryHistory = () => {
+  const { data: deliveredParcels, isLoading } = useGetDeliveredParcelsQuery();
+  if (isLoading) return <div>Loading parcels...</div>;
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold">Delivered parcels</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+        {deliveredParcels?.length ? (
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          deliveredParcels.map((parcel: any) => (
+            <ParcelCard key={parcel._id} parcel={parcel}></ParcelCard>
+          ))
+        ) : (
+          <div>No parcels found</div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ViewDeliveryHistory;
